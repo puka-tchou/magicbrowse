7c0e97cad608c0626300035a9529541567d90453
Gaspard Bariller <gaspard.bariller@tutanota.com>
2019-02-18 13:56:36 +0100

    Working simple WebGl plane

--- src/app/dashboard/card-single/card-single.component.ts
+++ src/app/dashboard/card-single/card-single.component.ts
@@ -1,7 +1,19 @@
-import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, ViewChild } from '@angular/core';
 import { MagicTheGatheringService } from 'src/app/shared/services/magic-the-gathering.service';
 import { SingleCardModel } from 'src/app/shared/models/magicthegathering/card-list/single-card.model';
 import { CardModel } from 'src/app/shared/models/magicthegathering/card-list/card.model';
+import {
+  Scene,
+  PerspectiveCamera,
+  WebGLRenderer,
+  MeshBasicMaterial,
+  Mesh,
+  PlaneGeometry,
+  Color,
+  TextureLoader,
+  DoubleSide
+} from 'three';
+import { OrbitControls } from 'three-orbitcontrols-ts';
 
 @Component({
   selector: 'app-card-single',
@@ -10,6 +22,7 @@ import { CardModel } from 'src/app/shared/models/magicthegathering/card-list/car
 })
 export class CardSingleComponent implements OnInit, OnChanges {
   @Input() imageUrl: string;
+  @ViewChild('container') div;
   public card: CardModel;
 
   constructor(private magicTheGatheringService: MagicTheGatheringService) {}
@@ -29,4 +42,49 @@ export class CardSingleComponent implements OnInit, OnChanges {
   }
 
   ngOnChanges() {}
+
+  ngAfterViewInit() {
+    this.createScene(this.div.nativeElement);
+  }
+
+  createScene(container) {
+    const scene = new Scene();
+    scene.background = new Color(0xffffff);
+
+    const camera = new PerspectiveCamera(
+      45,
+      window.innerWidth / window.innerHeight,
+      1,
+      1000
+    );
+
+    const controls = new OrbitControls(camera);
+
+    const renderer = new WebGLRenderer({
+      antialias: true,
+      alpha: true
+    });
+    renderer.setSize(500, 500);
+    container.appendChild(renderer.domElement);
+
+    const geometry = new PlaneGeometry();
+    const texture = new TextureLoader().load('assets/Image.jpeg');
+    const material = new MeshBasicMaterial({ map: texture, side: DoubleSide });
+    const plane = new Mesh(geometry, material);
+    scene.add(plane);
+
+    camera.position.z = 5;
+    controls.update();
+
+    const animate = function() {
+      requestAnimationFrame(animate);
+
+      // plane.rotation.x += 0.01;
+      // plane.rotation.y += 0.01;
+
+      renderer.render(scene, camera);
+    };
+
+    animate();
+  }
 }
